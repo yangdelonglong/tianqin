431a2f45eed2c47b00c984e71765557d83729c0c
yangdelong <601531098@qq.com>
2019-08-02 20:37:14 +0800

    补充js轮播图，ajax动态渲染echart数据

--- 天勤科技面试/js/pic3.js
+++ 天勤科技面试/js/pic3.js
@@ -1,10 +1,11 @@
 // 基于准备好的dom，初始化echarts实例
-var myChart = echarts.init(document.getElementById('main3'));
-var myChart1 = echarts.init(document.getElementById('main-4-l'));
-var myChart2 = echarts.init(document.getElementById('main-4-r'));
 
-// 指定图表的配置项和数据
-var option = {
+
+//ajax获取图表的动态数据
+function myData(text) {
+ 	console.log(text);
+ 	var myChart = echarts.init(document.getElementById('main3'));
+   	var option = {
     title: {
         text: '曲线图数据展示',
         x:'center',
@@ -32,7 +33,7 @@ var option = {
         {
             type : 'category',
             boundaryGap: ['20%', '20%'],
-            data : ['01/26','01/28','01/30','02/01','02/03','02/05','02/07', '02/09', '02/11', '02/13', '02/15', '02/17', '02/19', '02/21', '02/23']
+            data: text.data.xAxis
         }
     ],
     yAxis : [
@@ -61,107 +62,192 @@ var option = {
             lineStyle: {
             	color: '#4486ef',
             },
-            data:[8972, 6778, 7456, 5824, 8123, 300, 300, 5319, 7463, 1435, 9426, 8387, 8297, 445, 9135]
+            data:text.data.series
         }
     ]
 };
+ 	myChart.setOption(option);
+ }
 
-var option1 = {
-      title : {
-        text: '饼状图数据展示',
-        x:'center',
-        textStyle:{
-		　　fontWeight: 'normal',
-		　　color: '#000'
-		},
-    },
-    tooltip : {
-        trigger: 'item',
-        formatter: "{a} <br/>{b} : {c} ({d}%)"
-    },
-    series : [
-        {
-            name: '日期占比',
-            type: 'pie',
-            center: ['50%', '62%'],
-            data:[
-                {value:335, name:'Mon'},
-                {value:310, name:'Tud'},
-                {value:234, name:'Wed'},
-                {value:135, name:'Thu'},
-                {value:1548, name:'Fri'},
-                {value:1002, name:'Sat'},
-                {value:800, name:'Sun'}
-            ],
-            itemStyle: {
-                emphasis: {
-                    shadowBlur: 10,
-                    shadowOffsetX: 0,
-                    shadowColor: 'rgba(0, 0, 0, 0.5)'
-                }
-            }
-        }
-    ]
-};
+function myData2(text) {
+ 	console.log(text);
+ 	var textData = [];
+ 	for(var i = 0;i<text.data.xAxis.length;i++) {
+ 		textData.push({value:text.data.series[i], name:text.data.xAxis[i]});
+ 	}
+ 	var myChart1 = echarts.init(document.getElementById('main-4-l'));
+	var myChart2 = echarts.init(document.getElementById('main-4-r'));
+  	var option1 = {
+	    title : {
+	        text: '饼状图数据展示',
+	        x:'center',
+	        textStyle:{
+			　　fontWeight: 'normal',
+			　　color: '#000'
+			},
+	    },
+	    tooltip : {
+	        trigger: 'item',
+	        formatter: "{a} <br/>{b} : {c} ({d}%)"
+	    },
+	    series : [
+	        {
+	            name: '日期占比',
+	            type: 'pie',
+	            center: ['50%', '62%'],
+	            data:textData,
+	            itemStyle: {
+	                emphasis: {
+	                    shadowBlur: 10,
+	                    shadowOffsetX: 0,
+	                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+	                }
+	            }
+	        }
+	    ]
+	};
 
-var option2 = {
-	title: {
-		text: '柱状图数据展示',
-		x:'center',
-        textStyle:{
-		　　fontWeight: 'normal',
-		　　color: '#000'
+	var option2 = {
+		title: {
+			text: '柱状图数据展示',
+			x:'center',
+	        textStyle:{
+			　　fontWeight: 'normal',
+			　　color: '#000'
+			},
 		},
-	},
-    color: ['#3398DB'],
-    tooltip : {
-        trigger: 'axis',
-        axisPointer : {            // 坐标轴指示器，坐标轴触发有效
-            type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-        }
-    },
-    grid: {
-        left: '10%',
-        right: '10%',
-        bottom: '3%',
-        containLabel: true
-    },
-    xAxis : [
-        {
-            type : 'category',
-            data : ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-            axisTick: {
-                alignWithLabel: true
-            }, 
-        }
-    ],
-    yAxis : [
-        {
-            type : 'value',
-            name: '商品数',
-            axisLine: {
-            	show: false,
-            	lineStyle: {
-            		type: 'dashed'
-            	}
-            },
-            splitLine: {
-            	lineStyle: {
-            		type: 'dashed'
-            	}
-            }
-        }
-    ],
-    series : [
-        {
-            name:'当日合计',
-            type:'bar',
-            barWidth: '25%',
-            data:[10, 12, 8, 9, 8, 13, 5]
-        }
-    ]
-};
-// 使用刚指定的配置项和数据显示图表。
-myChart.setOption(option);
-myChart1.setOption(option1);
-myChart2.setOption(option2);
+	    color: ['#3398DB'],
+	    tooltip : {
+	        trigger: 'axis',
+	        axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+	            type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+	        }
+	    },
+	    grid: {
+	        left: '10%',
+	        right: '10%',
+	        bottom: '3%',
+	        containLabel: true
+	    },
+	    xAxis : [
+	        {
+	            type : 'category',
+	            data : text.data.xAxis,
+	            axisTick: {
+	                alignWithLabel: true
+	            }, 
+	        }
+	    ],
+	    yAxis : [
+	        {
+	            type : 'value',
+	            name: '商品数',
+	            axisLine: {
+	            	show: false,
+	            	lineStyle: {
+	            		type: 'dashed'
+	            	}
+	            },
+	            splitLine: {
+	            	lineStyle: {
+	            		type: 'dashed'
+	            	}
+	            }
+	        }
+	    ],
+	    series : [
+	        {
+	            name:'当日合计',
+	            type:'bar',
+	            barWidth: '25%',
+	            data:text.data.series
+	        }
+	    ]
+	};
+
+ 	myChart1.setOption(option1);
+	myChart2.setOption(option2);
+ }
+
+function myJson() {
+  	$.ajax({
+  		type:"get",
+  		url:"https://edu.telking.com/api/",
+  		data: {
+  			type: "month"
+  		},
+  		dataType: "jsonp",
+  		success: function(data) {
+  			myData(data)
+  		}
+  	})
+}
+function myJson2() {
+  	$.ajax({
+  		type:"get",
+  		url:"https://edu.telking.com/api/",
+  		data: {
+  			type: "week"
+  		},
+  		dataType: "jsonp",
+  		success: function(data) {
+  			myData2(data)
+  		}
+  	})
+}
+myJson();
+myJson2();
+
+//轮播
+var lunbo = document.getElementById("lunbo");
+var next = lunbo.getElementsByClassName("next")[0];
+var previous = lunbo.getElementsByClassName("previous")[0];
+var img = lunbo.getElementsByTagName("img")[0];
+var circles = lunbo.getElementsByClassName("lunbo-item");
+var pic_id = 3;
+function goNext () {
+	for(var i = 0; i<circles.length; i++) {
+		circles[i].className = "lunbo-item";
+	}
+	pic_id++;
+	if(pic_id == 6) {
+		pic_id = 1;
+	}
+	circles[pic_id-1].className = "lunbo-item sel";
+	img.src = "img/lunbo"+pic_id+".jpg";
+}
+function goBack () {
+	for(var i = 0; i<circles.length; i++) {
+		circles[i].className = "lunbo-item";
+	}
+	pic_id--;
+	if(pic_id == 0) {
+		pic_id = 5;
+	}
+	circles[pic_id-1].className = "lunbo-item sel";
+	img.src = "img/lunbo"+pic_id+".jpg";
+}
+next.onclick = function() {
+	goNext();
+}
+previous.onclick = function() {
+	goBack();
+}
+for(var i = 0; i<circles.length; i++) {
+	circles[i].setAttribute("itemId",i);
+	circles[i].onclick = function () {
+		for(var j= 0; j<circles.length; j++) {
+			circles[j].className = "lunbo-item";
+		}
+		this.className = "lunbo-item sel";
+		pic_id = parseInt(this.getAttribute("itemId"))+1;
+		img.src = "img/lunbo"+pic_id+".jpg";
+	}
+}
+var lun = setInterval(goNext,5000);
+lunbo.onmouseover = function() {
+	clearInterval(lun);
+}
+lunbo.onmouseout = function() {
+	lun = setInterval(goNext,5000);
+}
